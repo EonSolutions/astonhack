bb3c584d53a41fd2a845476b5e4b799be4067f5a
Cy4 <veschi@gmail.com>
2025-02-23 05:34:59 +0000

    Mapp

--- src/Map.jsx
+++ src/Map.jsx
@@ -7,6 +7,9 @@ import {
   Marker,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { FiClock } from "react-icons/fi";
+import { FaRegStar } from "react-icons/fa";
+import "./Map.css"; // Import the CSS file for styling
 
 const MapView = () => {
   const [position, setPosition] = useState([
@@ -38,7 +41,6 @@ const MapView = () => {
           }
 
           const data = await response.json();
-          console.log(data); // Log the response to check what you are receiving
 
           if (data.results) {
             setCharityShops(data.results);
@@ -54,52 +56,150 @@ const MapView = () => {
     }
   }, [position]);
 
-  console.log(charityShops)
+  console.log(charityShops);
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{ position: "absolute", top: 0, right: 0, bottom: 0, left: 0 }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-      />
-      <CircleMarker
-        center={position}
-        radius={8} // Size of the circle
-        pathOptions={{
-          fillColor: "#4287f5", // Fill color (inside the circle)
-          color: "#333", // Border color
-          weight: 2, // Border width
-          opacity: 1, // Border opacity
-          fillOpacity: 1, // Fill opacity
+    <>
+      <header className="wardrobe-header">Sell My Clothes</header>
+      <div
+        className="map-container"
+        style={{
+          backgroundColor: "#f5f5f5",
+          height: "calc(100vh - 70px)",
+          marginTop: "70px",
         }}
       >
-        <Popup>You are here.</Popup>
-      </CircleMarker>
-      {charityShops ? (
-      charityShops.map((shop, i) => (
-        <Marker
-          key={i}
-          position={{
-            lat: shop.geometry.location.lat,
-            lng: shop.geometry.location.lng,
+        <div className="sidebar">
+          {charityShops.map((shop, i) => (
+            <a
+              key={i}
+              href={`https://www.google.com/maps/search/?api=1&query=${shop.geometry.location.lat},${shop.geometry.location.lng}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="shop-card"
+            >
+              {shop.photos && shop.photos.length > 0 && (
+                <img
+                  src={`https://maps.googleapis.com/maps/api/place/photo?photoreference=${
+                    shop.photos[0].photo_reference
+                  }&sensor=false&maxheight=200&maxwidth=200&key=${
+                    import.meta.env.VITE_GOOGLE_KEY
+                  }`}
+                  alt={shop.name}
+                  className="shop-image"
+                />
+              )}
+              <div className="shop-info">
+                <h3>{shop.name}</h3>
+                <p>
+                  <FiClock style={{ marginRight: "4px" }} />
+                  {shop.opening_hours ? (
+                    <>Open now: {shop.opening_hours.open_now ? "Yes" : "No"}</>
+                  ) : (
+                    <>Opening times not available</>
+                  )}
+                </p>
+                <p>
+                  <FaRegStar style={{ marginRight: "4px" }} />
+                  {shop.rating ? (
+                    <>{shop.rating} Stars</>
+                  ) : (
+                    <>Rating not available</>
+                  )}
+                </p>
+              </div>
+            </a>
+          ))}
+        </div>
+        <div
+          style={{
+            position: "relative",
+            width: "calc(100vw - 300px)",
+            height: "100%",
           }}
         >
-          <Popup>
-            <strong>{shop.name}</strong>
-            <br />
-            {shop.opening_hours ? (
-                <strong>Open now: {shop.opening_hours.open_now ? "Yes" : "No"}</strong>
+          <MapContainer
+            center={position}
+            zoom={13}
+            maxZoom={18} // Set maximum zoom level
+            minZoom={10} // Set minimum zoom level
+            style={{
+              position: "absolute",
+              top: "50px",
+              right: "50px",
+              bottom: "50px",
+              left: "50px",
+              borderRadius: "40px",
+              boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+            }}
+            zoomControl={false} // Hide zoom controls
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+            />
+            <CircleMarker
+              center={position}
+              radius={8} // Size of the circle
+              pathOptions={{
+                fillColor: "#4287f5", // Fill color (inside the circle)
+                color: "#333", // Border color
+                weight: 2, // Border width
+                opacity: 1, // Border opacity
+                fillOpacity: 1, // Fill opacity
+              }}
+            >
+              <Popup>You are here.</Popup>
+            </CircleMarker>
+            {charityShops ? (
+              charityShops.map((shop, i) => (
+                <CircleMarker
+                  key={i}
+                  center={[
+                    shop.geometry.location.lat,
+                    shop.geometry.location.lng,
+                  ]}
+                  radius={8} // Size of the circle
+                  pathOptions={{
+                    fillColor: "#28c953", // Fill color (inside the circle)
+                    color: "#333", // Border color
+                    weight: 2, // Border width
+                    opacity: 1, // Border opacity
+                    fillOpacity: 1, // Fill opacity
+                  }}
+                >
+                  <Popup>
+                    <div style={{ marginBottom: "4px", maxWidth: "200px" }}>
+                      <strong>{shop.name}</strong>
+                    </div>
+                    <span style={{ display: "flex", alignItems: "center" }}>
+                      <FiClock style={{ marginRight: "4px" }} />
+                      {shop.opening_hours ? (
+                        <>
+                          Open now: {shop.opening_hours.open_now ? "Yes" : "No"}
+                        </>
+                      ) : (
+                        <>Opening times not available</>
+                      )}
+                    </span>
+                    <span style={{ display: "flex", alignItems: "center" }}>
+                      <FaRegStar style={{ marginRight: "4px" }} />
+                      {shop.rating ? (
+                        <>{shop.rating} Stars</>
+                      ) : (
+                        <>Rating not available</>
+                      )}
+                    </span>
+                  </Popup>
+                </CircleMarker>
+              ))
             ) : (
-              <p>Opening times not available</p>
+              <p>Loading charity shops...</p>
             )}
-          </Popup>
-        </Marker>
-      ))) : <p>Loading charity shops...</p>}
-    </MapContainer>
+          </MapContainer>
+        </div>
+      </div>
+    </>
   );
 };
 
